a5e6a452fa8bc5efeabb54070c8af67c4a80ad81
Janish Singh <69780925+JanishIT@users.noreply.github.com>
2026-06-02 17:42:07 -0600

    f

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
+const toggle=document.getElementById('theme-toggle');
+
+toggle.addEventListener('click',()=>{
+
+document.body.classList.toggle('light');
+
+if(document.body.classList.contains('light')){
+toggle.innerHTML='☀️';
+}
+else{
+toggle.innerHTML='🌙';
+}
+
+});
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
+
 anchor.addEventListener('click',function(e){
+
 e.preventDefault();
-document.querySelector(this.getAttribute('href')).scrollIntoView({
+
+document.querySelector(
+this.getAttribute('href')
+).scrollIntoView({
 behavior:'smooth'
 });
+
 });
+
 });
